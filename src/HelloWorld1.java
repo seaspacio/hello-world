718db24c74ee425472593402fe0e7aabc155d71a
seaspacio <44484142+seaspacio@users.noreply.github.com>
2018-10-31 00:48:01 +0300

    Add files via upload

---
+++ src/HelloWorld1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author DELL INSPIRON
+ *
+ */
+public class HelloWorld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World again !");
+	}
+
+}
